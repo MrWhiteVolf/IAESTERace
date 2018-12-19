5a4cd94b7521a1c19bcf36f05edf125564c83eaf
MrWhiteVolf <patrykszynal97@gmail.com>
2018-12-19 08:10:17 +0100

    DB creation script

---
+++ CreateDatabaseTables.sql
@@ -0,0 +1,101 @@
+CREATE DATABASE IF NOT EXISTS IAESTE_RACE;
+
+USE IAESTE_RACE;
+
+CREATE TABLE IF NOT EXISTS USER
+(
+  Id integer NOT NULL AUTO_INCREMENT,
+  Name nvarchar(256) NOT NULL,
+  Surname nvarchar(256) NOT NULL,
+  Email nvarchar(256) NOT NULL,
+  Phone integer NOT NULL,
+  AboutMe nvarchar(2048) NULL,
+  TeamId integer NOT NULL,
+  PRIMARY KEY (Id)
+);
+
+CREATE TABLE IF NOT EXISTS TEAM
+(
+  Id integer NOT NULL AUTO_INCREMENT,
+  Name nvarchar(256) NOT NULL,
+  Description nvarchar(2048) NULL,
+  First_User_Id integer,
+  Sec_User_Id integer,
+  PRIMARY KEY (Id),
+  FOREIGN KEY (First_User_Id) REFERENCES USER(Id),
+  FOREIGN KEY (Sec_User_Id) REFERENCES USER(Id)
+);
+
+SELECT
+    COUNT(*)
+INTO
+    @num
+FROM
+    `information_schema`.`table_constraints`
+WHERE
+    `table_schema` = 'IAESTE_RACE'
+    AND `table_name` = 'USER'
+    AND `constraint_name` = 'FK_TeamId'
+    AND `constraint_type` = 'FOREIGN KEY';
+
+SET @statement := IF(
+    @num > 0,
+    'ALTER TABLE USER ADD CONSTRAINT FK_TeamId FOREIGN KEY (TeamId) REFERENCES TEAM(Id);',
+    'ALTER TABLE USER;'
+);
+PREPARE statement FROM @statement;
+EXECUTE statement;
+
+CREATE TABLE IF NOT EXISTS CONTACT
+(
+  Id integer NOT NULL AUTO_INCREMENT,
+  Name nvarchar(256),
+  PhoneNum integer,
+  PRIMARY KEY (Id)
+);
+
+CREATE TABLE IF NOT EXISTS CURIOSITY
+(
+  Id integer NOT NULL AUTO_INCREMENT,
+  Title nvarchar(256),
+  Cur_Text nvarchar(2048),
+  PhotoId integer,
+  PRIMARY KEY (Id)
+);
+
+CREATE TABLE IF NOT EXISTS LOCALIZATION
+(
+  Id integer NOT NULL AUTO_INCREMENT,
+  TeamId integer NOT NULL,
+  Lat float(8,5),
+  Lon float(8,5),
+  Time DATETIME,
+  PRIMARY KEY (Id),
+  FOREIGN KEY (TeamId) REFERENCES TEAM (Id)
+);
+
+CREATE TABLE IF NOT EXISTS CONVERSATION
+(
+  Id integer NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (Id)
+);
+
+CREATE TABLE IF NOT EXISTS MESSAGE
+(
+  Id integer NOT NULL AUTO_INCREMENT,
+  UserId integer NOT NULL,
+  Mes_Text nvarchar(2048),
+  Time DATETIME,
+  ConvId integer,
+  PRIMARY KEY (Id),
+  FOREIGN KEY (UserId) REFERENCES USER(Id),
+  FOREIGN KEY (ConvId) REFERENCES CONVERSATION(Id)
+);
+
+CREATE TABLE IF NOT EXISTS TEAM_CONV_ASOC
+(
+  ConvId integer,
+  TeamId integer,
+  FOREIGN KEY (ConvId) REFERENCES CONVERSATION(Id),
+  FOREIGN KEY (TeamId) REFERENCES TEAM(Id)
+);
